cba8246b817eca7bb1843b7a01846c9a029fa460
2391288752@qq.com <2391288752@qq.com>
2018-11-26 22:07:26 +0800

    add cloud.miniapp.com-API & user codes

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 //app.js
-
+import myconfig from 'config'
+import user from 'utils/user'
 App({
-  onLaunch: function () {
-    
+  onLaunch: function () { 
+    let that = this
+    // 引入 BaaS SDK
+    require('./utils/sdk-v1.4.0')
+    let clientId = this.globalData.clientId
+    wx.BaaS.init(clientId)
+    // 登陆并获取当前用户信息存储到globaldata里
+    wx.BaaS.login(false).then(res => {
+      user.getPerInfos(wx.BaaS.storage.get('uid'), (res) => {
+        this.globalData.nowUserInfos = res.data.objects
+        console.log(this.globalData.nowUserInfos)
+      })
+    })  
   },
-  globalData: {
-    
-  }
+  globalData: { 
+    clientId: myconfig.myconfig.clientId,
+    PerInfoId: myconfig.myconfig.PerInfoId,
+    tableId: myconfig.myconfig.tableId,
+    nowUserInfos: [],
+  },
+ 
 })
--- pages/my/my.js
+++ pages/my/my.js
@@ -1,21 +1,56 @@
 // pages/my/my.js
-
+var app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    'hasWeInfo': false
   },
 
+// 点击绑定微信号将头像等信息存入缓存
+  userInfoHandler(data) {
+    wx.BaaS.handleUserInfo(data).then(res => {
+      var personWeInfo = res
+      wx.setStorage({
+        key: 'personWeInfo',
+        data: personWeInfo,
+        success: function (res) {
+        }
+      })
+      this.setData({
+        'hasWeInfo': true,
+        'personPortrait': res.avatarUrl,
+        'personNick': res.nickName
+      })
+    })
+  },
+  // 获取缓存，是否用户已经授权
+  getPersonWeInfo: function () {
+    var value = wx.getStorageSync('personWeInfo')
+    if (!(value === '')) {
+      this.setData({
+        'hasWeInfo': true,
+        'personPortrait': value.avatarUrl,
+        'personNick': value.nickName
+      })
+    }
+  },
+// 编辑资料跳转到个人资料页面
+  editPerInfo: function () {
+    wx.navigateTo({
+      url: 'user/user',
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.getPersonWeInfo()
   },
 
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -27,7 +62,8 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+     // 从globalData获取当前用户信息
+    console.log(app.globalData.nowUserInfos)
   },
 
   /**
