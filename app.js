61837932adf6c8a3398c8d03e8f72628db1beea9
2391288752@qq.com <2391288752@qq.com>
2019-01-20 02:20:53 +0800

    rebase后

--- app.js
+++ app.js
@@ -2,15 +2,7 @@
 import myconfig from 'config'
 import user from 'utils/user'
 App({
-// 获取用户全部信息并放在globalData里
-fetchAllUser(){
-  unitfunction.getList(this.globalData.PerInfoId, (res) => {
-    this.globalData.allUserList = res.data.objects
-    console.log(this.globalData)
-  })
-},
-
-  onLaunch: function () { 
+  onLaunch: function () {
     let that = this
     // 引入 BaaS SDK
     require('./utils/sdk-v1.4.0')
@@ -20,26 +12,16 @@ fetchAllUser(){
     wx.BaaS.login(false).then(res => {
       user.getPerInfos(wx.BaaS.storage.get('uid'), (res) => {
         this.globalData.nowUserInfos = res.data.objects
-      })
-    })  
-    // 登陆
-    wx.BaaS.login(false).then(res => {
-    })
-    this.fetchAllUser()
-    // 登陆并获取当前用户信息存储到globaldata里
-    wx.BaaS.login(false).then(res => {
-      user.getPerInfos(wx.BaaS.storage.get('uid'), (res) => {
-        this.globalData.nowUserInfos = res.data.objects
         console.log(this.globalData.nowUserInfos)
       })
-    })  
+    })
   },
-  globalData: { 
+  globalData: {
     clientId: myconfig.myconfig.clientId,
     PerInfoId: myconfig.myconfig.PerInfoId,
     tableId: myconfig.myconfig.tableId,
     famousId: myconfig.myconfig.famousId,
-    nowUserInfos: [],   
+    nowUserInfos: [],
   },
 
 })
--- utils/bookoperation.js
+++ utils/bookoperation.js
@@ -1,4 +1,3 @@
-
 // 获取书列表
 let getList = (ctx, cb) => {
   let tableId = ctx,
@@ -22,27 +21,3 @@ module.exports = {
   getList,
   getBook
 }
-// 获取书列表
-let getList = (ctx, cb) => {
-  let tableId = ctx,
-    Books = new wx.BaaS.TableObject(tableId)
-  Books.find()
-    .then(res => cb(res))
-    .catch(err => console.dir(err))
-}
-
-// 获取单本书的信息
-let getBook = (ctx, cb) => {
-  let tableId = getApp().globalData.tableId
-  let Books = new wx.BaaS.TableObject(tableId)
-  let myRecord = ctx.data.curRecordId.trim()
-  Books.get(myRecord).then(res => cb(res))
-    .catch(err => console.dir(err))
-}
-
-
-module.exports = {
-  getList,
-  getBook
-}
-
